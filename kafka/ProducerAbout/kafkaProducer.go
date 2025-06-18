01b6e4b09d8998bc7e8a147e753397ebf02ff430
PC-20231121BZBH\Administrator <443500251@qq.com>
2025-06-18 10:07:02 +0800

    no message

--- kafka/ProducerAbout/kafkaProducer.go
+++ kafka/ProducerAbout/kafkaProducer.go
@@ -22,6 +22,25 @@ func (kp *KafkaProducer) Send(msg_str string) (partition int32, offset int64, er
 	return
 }
 
+func (kp *KafkaProducer) SendwithEbaseRequestID(msg_str string, EbaseRequestID string) (partition int32, offset int64, err error) {
+
+	kafka_msg := &sarama.ProducerMessage{
+		Topic: kp.Topic,
+		//newManualPartitioner: true  #是否手动分配分区
+		//如果手动分区选择true ，需要手动设置分区 增加如下代码
+		//Partition: int32(your_partition_number),  // 设置分区号
+		Value: sarama.StringEncoder(msg_str),
+		Headers: []sarama.RecordHeader{
+			{
+				Key:   []byte("EbaseRequestID"),
+				Value: []byte(EbaseRequestID),
+			},
+		},
+	}
+	partition, offset, err = (*kp.Sp).SendMessage(kafka_msg)
+	return
+}
+
 // 批量发送
 func (kp *KafkaProducer) SendBatch(msg_strs []string) (err error) {
 
